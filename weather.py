cad7bb9ad7f722eaccbbd718cfdea761b420db91
Adwoa-p <pokuaaa158@gmail.com>
2024-08-23 10:59:57 +0100

    modified api key

--- weather.py
+++ weather.py
@@ -1,38 +1,44 @@
 import pyowm
+import os
+from dotenv import load_dotenv
 
-# owm=pyowm.OWM('584375f9faf6841863ba138f9e1b599d')
-# mgr=owm.weather_manager()
-# observation = mgr.weather_at_place('London,uk')
-# w = observation.weather
-# print(w.wind(),w.humidity)
+load_dotenv() 
+api_key=os.getenv('api_key')
 
 
-# owm=pyowm.OWM('584375f9faf6841863ba138f9e1b599d')
-# mgr=owm.weather_manager()
-# observation = mgr.weather_at_place('Hong Kong,china')
-# w = observation.weather
-# print(f"The humidity in Hong Kong is {w.humidity}")
+owm=pyowm.OWM(api_key)
+mgr=owm.weather_manager()
+observation = mgr.weather_at_place('London,uk')
+w = observation.weather
+print(w.wind(),w.humidity)
 
-# owm=pyowm.OWM('584375f9faf6841863ba138f9e1b599d')
-# mgr=owm.weather_manager()
-# observation = mgr.weather_at_place('Tokyo,japan')
-# w = observation.weather
-# print(w.temperature())
 
-# owm=pyowm.OWM('584375f9faf6841863ba138f9e1b599d')
-# mgr=owm.weather_manager()
-# observation = mgr.weather_at_place('LaPaz,bolivia')
-# w = observation.weather
-# print(w.pressure)
+owm=pyowm.OWM(api_key)
+mgr=owm.weather_manager()
+observation = mgr.weather_at_place('Hong Kong,china')
+w = observation.weather
+print(f"The humidity in Hong Kong is {w.humidity}")
 
-# owm=pyowm.OWM('584375f9faf6841863ba138f9e1b599d')
-# mgr=owm.weather_manager()
-# observation = mgr.weather_at_place('Koforidua,ghana')
-# w = observation.weather
-# print(w.wind())
+owm=pyowm.OWM(api_key)
+mgr=owm.weather_manager()
+observation = mgr.weather_at_place('Tokyo,japan')
+w = observation.weather
+print(w.temperature())
+
+owm=pyowm.OWM(api_key)
+mgr=owm.weather_manager()
+observation = mgr.weather_at_place('LaPaz,bolivia')
+w = observation.weather
+print(w.pressure)
+
+owm=pyowm.OWM(api_key)
+mgr=owm.weather_manager()
+observation = mgr.weather_at_place('Koforidua,ghana')
+w = observation.weather
+print(w.wind())
 
 from pprint import pprint
 import requests
-r=requests.get('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=584375f9faf6841863ba138f9e1b599d')
+r=requests.get('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=api_key')
 pprint(r.json())
 
